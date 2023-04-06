3b392a14ab0de86d77fd55c2262ed2756e647056
Hein Tay Zar <heintayzarhm@gmail.com>
2023-04-06 16:07:00 +0630

    Fix styling

--- src/components/messages/ErrorMessage.js
+++ src/components/messages/ErrorMessage.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { IoIosAlert } from 'react-icons/io';
 
 const ErrorMessage = ({ message }) => (
-  <div className="bg-red-600 p-2 my-2 animate-left text-white w-full flex justify-center items-center gap-3" role="alert">
+  <div className="bg-red-600 p-2 animate-left text-white w-full flex justify-center items-center gap-3" role="alert">
     <IoIosAlert />
     {message}
   </div>
