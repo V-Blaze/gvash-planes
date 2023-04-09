b42d50caef115a4eb345e08a8df62adb62f31fac
V-Blaze <valentine6586@gmail.com>
2023-04-09 13:23:58 +0100

    Update Addplane styles

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     }
   });
   return (
-    <div className="App font-primary bg-gray-50 relative p-6 pt-40 min-h-screen">
+    <div className="App font-primary">
       {alert && <ErrorMessage message={alert} />}
       {notice && <NoticeMessage message={notice} /> }
       <Routes>
--- src/pages/AddPlane/AddPlane.js
+++ src/pages/AddPlane/AddPlane.js
@@ -94,12 +94,12 @@ const AddPlane = () => {
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900 ">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
         <h1 className="flex font-secondary items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2 pt-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
           GVASH PLANES
         </h1>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 px-[10%] md:px-[25%] dark:bg-gray-800 dark:border-gray-700">
+        <div className=" bg-white rounded-lg shadow dark:border md:mt-0 px-[10%] md:px-[25%] dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create New plane
