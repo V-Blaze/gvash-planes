8f55bbe7f376c69dc4423db00702f0893501116c
Abdessittir <harkati.web.dev@gmail.com>
2023-04-12 11:43:58 +0000

    Add user name and fix some styles

--- src/pages/reservations/Resvervations.js
+++ src/pages/reservations/Resvervations.js
@@ -4,10 +4,12 @@ import { useEffect } from 'react';
 import { reservationsThunk } from '../../slices/reservationSlice/reservationSlice';
 import { setAlert } from '../../slices/appSlice/appSlice';
 import ReservationCard from './ReservationCard';
+import Loading from '../../components/Loading/Loading';
 
 export default function Reservations() {
   const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
   const reservations = useSelector((state) => state.reservation.reservations);
   const loading = useSelector((state) => state.reservation.loading);
 
@@ -20,12 +22,16 @@ export default function Reservations() {
   }, [dispatch, token]);
 
   if (loading) {
-    return <h2>Loading...</h2>;
+    return <Loading />;
   }
 
   return (
     <div>
       <h1 className="reservations_title font-secondary">My Reservations</h1>
+      <div className="greeting">
+        <h2>Hello {user.name}</h2>
+        {reservations.length ===0 && <p>You have No reservation yet</p>}
+      </div>
       <div className="reservation_container">
         {reservations.map((data) => (
           <ReservationCard
